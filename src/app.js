1259eb255a9b0081941de7bcdc11d1c5184afad7
Andó Attila <andoattila698@gmail.com>
2023-03-23 13:20:05 +0100

    Tka

---
+++ src/app.js
@@ -0,0 +1,45 @@
+let baseInput = document.querySelector('#base')
+let heightInput = document.querySelector('#height')
+let areaInput = document.querySelector('#area')
+let calcButton = document.querySelector('#calcButton')
+
+//console.log(baseInput, heightInput, areaInput, calcButton)
+
+if(calcButton){
+    calcButton.addEventListener('click', () => {
+   
+        workInput()
+      
+    
+    })
+}
+
+
+function workInput(){
+    let base =Number(baseInput.value)
+    let height = Number(heightInput.value)
+    let area = calcTriangleArea(base, height)
+    areaInput.value = area
+    console.log(area)
+}
+
+
+
+function calcTriangleArea(base,height){
+    if (base <= 0 || height <=0){
+        throw new Error('Hiba! A paraméter nem megfelelő');
+    }
+    return base * height / 2;
+}
+
+
+function checkInput(input){
+    //Szabályos kifejezések
+    let res = input.match(/^[0-9]+$/);
+    if(res){
+        return true;
+    }else{
+        return false;
+    }
+
+}
